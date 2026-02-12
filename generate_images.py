18766f31a1fa3faa3d54d9b770e317f2e89f96dd
Andrew Pimenta <167480888+AndrewLPimenta@users.noreply.github.com>
2026-02-12 11:31:00 -0300

    Update generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -101,25 +101,27 @@ async def main() -> None:
     """
     access_token = os.getenv("ACCESS_TOKEN")
     if not access_token:
-        # access_token = os.getenv("GITHUB_TOKEN")
         raise Exception("A personal access token is required to proceed!")
-    user = os.getenv("GITHUB_ACTOR")
-    if user is None:
-        raise RuntimeError("Environment variable GITHUB_ACTOR must be set.")
+
+    # 🔥 FORÇANDO SEU USUÁRIO
+    user = "AndrewLPimenta"
+
     exclude_repos = os.getenv("EXCLUDED")
     excluded_repos = (
         {x.strip() for x in exclude_repos.split(",")} if exclude_repos else None
     )
+
     exclude_langs = os.getenv("EXCLUDED_LANGS")
     excluded_langs = (
         {x.strip() for x in exclude_langs.split(",")} if exclude_langs else None
     )
-    # Convert a truthy value to a Boolean
+
     raw_ignore_forked_repos = os.getenv("EXCLUDE_FORKED_REPOS")
     ignore_forked_repos = (
         not not raw_ignore_forked_repos
         and raw_ignore_forked_repos.strip().lower() != "false"
     )
+
     async with aiohttp.ClientSession() as session:
         s = Stats(
             user,
